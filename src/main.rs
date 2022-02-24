4d8d981dec14c154273ac310700f58f25167cf41
Darwin Subramaniam <darwinsubramaniam@googlemail.com>
2022-02-24 20:20:28 +0800

    rounding and correction on the prod.sample.env

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 mod lib;
-use clap::{Parser};
+use clap::Parser;
 use lib::MainTokenFiat;
 
 #[derive(Parser, Debug)]
 #[clap(author,version,about,long_about=None)]
 struct Args {
-    #[clap(short, long, help = "Choose USD as Fiat" )]
+    #[clap(short, long, help = "Choose USD as Fiat")]
     usd: bool,
     #[clap(short, long, help = "Choose SGD as Fiat")]
     sgd: bool,
@@ -13,19 +13,24 @@ struct Args {
     pundix: bool,
     #[clap(short, long, help = "Choose FUNCTIONX as Crypto")]
     functionx: bool,
-    #[clap(short, long, help= "Ammount to be converted")]
+    #[clap(short, long, help = "Ammount to be converted")]
     ammount: f64,
-    #[clap(short('c'),long("c2f"), help= "Flag this to make conversion from crypto to fiat")]
-    crypto_to_fiat:bool,
+    #[clap(
+        short('c'),
+        long("c2f"),
+        help = "Flag this to make conversion from crypto to fiat"
+    )]
+    crypto_to_fiat: bool,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     match dotenv::from_filename("prod.env") {
-        Ok(_) => {},
-        Err(_) => { 
-            dotenv::from_filename("sandbox.env")
-            .expect("Missing env file, you will need prod.env(Production) or sandbox.env(Dev).");
+        Ok(_) => {}
+        Err(_) => {
+            dotenv::from_filename("sandbox.env").expect(
+                "Missing env file, you will need prod.env(Production) or sandbox.env(Dev).",
+            );
             println!("Application is running in Sandbox Mode.");
         }
     };
@@ -56,28 +61,56 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-async fn display(crypto: MainTokenFiat, fiat: MainTokenFiat, args:Args) {
+async fn display(crypto: MainTokenFiat, fiat: MainTokenFiat, args: Args) {
     let price = lib::latest_price::Price::get_quotes(crypto, fiat)
         .await
         .unwrap();
-
-        println!("Price of 1 {crypto} :: [{price} {fiat}]");
+    let price = round_2(price);
+    
+    println!("Price of 1 {crypto} :: [{price} {fiat}]");
 
     match &args.crypto_to_fiat {
         true => {
             let conversion = conversion(args.ammount, price, false);
-            println!("With {} {} -> you will get {:.2} {}", args.ammount,&crypto,conversion, &fiat)
+            println!(
+                "With {} {} -> you will get {} {}",
+                args.ammount, &crypto, conversion, &fiat
+            )
         }
         false => {
             let conversion = conversion(args.ammount, price, true);
-            println!("With {} {} -> you will get {:.2} {}", args.ammount,&fiat,conversion, &crypto)
+            println!(
+                "With {} {} -> you will get {} {}",
+                args.ammount, &fiat, conversion, &crypto
+            )
         }
     };
 }
 
-fn conversion(ammount:f64, price:f64,reverse:bool) -> f64{
+fn conversion(ammount: f64, price: f64, reverse: bool) -> f64 {
     match reverse {
-        true => ammount / price,
-        false => price * ammount
+        true => round_2(ammount / price),
+        false => round_2(price * ammount),
+    }
+}
+
+fn round_2(value: f64) -> f64 {
+    (value * 100.0).round() / 100.0
+}
+
+#[cfg(test)]
+mod test {
+    use super::conversion;
+
+    #[test]
+    fn conversion_test() {
+        let convert = conversion(100.0, 0.5, false);
+        assert_eq!(50.0, convert);
+        let convert = conversion(100.0, 0.5, true);
+        assert_eq!(200.0, convert);
+        let convert = conversion(45.1, 0.51474, false);
+        assert_eq!(23.21, convert);
+        let convert = conversion(45.1, 0.51474, true);
+        assert_eq!(87.62, convert);
     }
 }
